924c3141fd1741740140d5ef56bb90b9710bfef3
ZhouDaxia <zhoudongxu929@163.com>
2016-09-21 19:37:28 +0800

    MyAnswer_01_最后提交

--- MyAnswer_01/leptjson.c
+++ MyAnswer_01/leptjson.c
@@ -20,6 +20,7 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
         return LEPT_PARSE_INVALID_VALUE;
     c->json += 3;
+    /*
 	const char *p = c->json;
 	while (*p != '\0') {
 		if (*p != ' ' || *p != '\t' || *p != '\n' || *p != '\r') {
@@ -27,7 +28,26 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
 		}
 		p++;
 	}
+    */
     v->type = LEPT_NULL;
+    return LEPT_PARSE_OK;
+}
+
+static int lept_parse_true(lept_context* c, lept_value* v){
+    EXPECT(c, 't');
+    if(c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_TRUE;
+    return LEPT_PARSE_OK; 
+}
+
+static int lept_parse_false(lept_context* c, lept_value* v){
+    EXPECT(c,'f');
+    if(c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 4;
+    v->type = LEPT_FALSE;
     return LEPT_PARSE_OK;
 }
 
@@ -35,20 +55,48 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':  return lept_parse_null(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
+        case 't': return lept_parse_true(c,v);
+        case 'f': return lept_parse_false(c, v);
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
 }
 
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
+    int ret;
     assert(v != NULL);
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+        lept_parse_whitespace(&c);
+        if (*c.json != '\0')
+            ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    return ret;
 }
 
 lept_type lept_get_type(const lept_value* v) {
     assert(v != NULL);
     return v->type;
 }
+
+
+//另外一种解析方法，不要在lept_parse_null里写，直接在lept_parse里更改（这种方式更好，因为原来的只检测了前两部分）
+//代码如下
+/*
+int lept_parse(lept_value* v, const char* json) {
+    lept_context c;
+    int ret;
+    assert(v != NULL);
+    c.json = json;
+    v->type = LEPT_NULL;
+    lept_parse_whitespace(&c);
+    if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+        lept_parse_whitespace(&c);
+        if (*c.json != '\0')
+            ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    return ret;
+}
+*/
